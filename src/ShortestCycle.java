3e90a8af6084929ed803c3581b9f5559e41de07b
Михаил Зулкарнеев <zulkarneev@mail.ru>
2017-03-03 17:35:56 +0300

    First commit

---
+++ src/ShortestCycle.java
@@ -0,0 +1,16 @@
+
+import edu.princeton.cs.algs4.*;
+
+public class ShortestCycle {
+	Stack<Integer> stack;
+	
+	public ShortestCycle(Digraph graph) {
+		
+	}
+	
+	public Iterable<Integer> getShortestCycle() {
+		return stack;		
+	} 
+	
+	
+}
